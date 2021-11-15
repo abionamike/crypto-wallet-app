c0bae158d5c0a4ac900acde568e2adf12545e24f
Michael Abiona <amabiona21@gmail.com>
2021-11-15 11:13:07 +0100

    finiished working on the home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
     View,
-    Text
+    Text,
+    FlatList,
+    TouchableOpacity,
+    Image
 } from 'react-native';
 import { MainLayout  } from '.';
 import { useDispatch, useSelector } from 'react-redux';
@@ -13,6 +16,9 @@ import { BalanceInfo, Chart, IconTextButton  } from '../components';
 const Home = () => {
     const dispatch = useDispatch();
     const { myHoldings, coins } = useSelector(state => state.marketReducer);
+
+    const [selectedCoin, setSelectedCoin] = useState(null);
+
     useFocusEffect(
         useCallback(() => {
             dispatch(getHoldings(dummyData.holdings));
@@ -92,7 +98,75 @@ const Home = () => {
                     containerStyle={{
                         marginTop: SIZES.padding * 2
                     }}
-                    chartPrices={coins[0]?.sparkline_in_7d?.price}
+                    chartPrices={selectedCoin ? selectedCoin?.sparkline_in_7d?.price : coins[0]?.sparkline_in_7d?.price}
+                />
+
+                {/* Top Cryptocurrency */}
+                <FlatList 
+                    data={coins}
+                    keyExtractor={item => item.id}
+                    contentContainerStyle={{
+                        marginTop: 30,
+                        paddingHorizontal: SIZES.padding,
+                    }}
+                    ListHeaderComponent={
+                        <View style={{ marginBottom: SIZES.radius }}>
+                            <Text style={{ color: COLORS.white, ...FONTS.h3, fontSize: 18 }}>Top Cryptocurrency</Text>
+                        </View>
+                    }
+                    renderItem={({ item }) => {
+                        const priceColor = (item.price_change_percentage_7d_in_currency == 0) ? COLORS.lightGray3 : (item.price_change_percentage_7d_in_currency > 0) ? COLORS.lightGreen : COLORS.red;
+                        return (
+                            <TouchableOpacity
+                                style={{
+                                    height: 55,
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                                onPress={() => setSelectedCoin(item)}
+                            >
+                                {/* Logo */}
+                                <View 
+                                    style={{ width: 35 }}
+                                >
+                                    <Image 
+                                        source={{ uri: item.image }}
+                                        style={{ height: 20, width: 20  }}
+                                    />
+                                </View>
+    
+                                {/* Name */}
+                                <View style={{ flex: 1 }} >
+                                    <Text style={{ color: COLORS.white, ...FONTS.h3 }}>{item.name}</Text>
+                                </View>
+    
+                                {/* Figures */}
+                                <View>
+                                    <Text style={{ textAlign: 'right', color: COLORS.white, ...FONTS.h4 }}>$ {item.current_price}</Text>
+                                    <View
+                                        style={{
+                                            flexDirection: 'row',
+                                            alignItems: 'center',
+                                            justifyContent: 'flex-end'
+                                        }}
+                                    >
+                                        {item.price_change_percentage_7d_in_currency != 0 && 
+                                            <Image 
+                                                source={icons.upArrow}
+                                                style={{ height: 10, width: 10, tintColor: priceColor, transform: item.price_change_percentage_7d_in_currency > 0 ? [{ rotate: '45deg' }] : [{ rotate: '125deg' }] }}
+                                            />
+                                        }
+                                        <Text style={{ marginLeft: 5, color: priceColor, ...FONTS.body5, lineHeight: 15 }}>{item.price_change_percentage_7d_in_currency.toFixed(2)}%</Text>
+                                    </View>
+                                </View>
+                            </TouchableOpacity>
+                        )
+                    }}
+
+                    ListFooterComponent ={
+                        <View style={{ marginBottom: 50 }} />
+                    }
                 />
             </View>
         </MainLayout>
--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    FlatList,
+    Image
 } from 'react-native';
 import { MainLayout } from '.';
+import { useDispatch, useSelector  } from 'react-redux';
+import { useFocusEffect } from '@react-navigation/native ';
+import { getHoldings } from '../store/market/marketAction';
+import { BalanceInfo, Chart } from '../components';
+import { COLORS, FONTS, SIZES } from '../constants';
+// import {} from 
 
 const Portfolio = () => {
+    const dispatch = useDispatch();
+    const { myHoldings } = useSelector(state => state.marketReducer);
+
     return (
         <MainLayout>
             <View>
